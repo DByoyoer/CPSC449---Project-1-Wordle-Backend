fa25b5e67e45f89cc66fd377eed05abe364ac9b3
Michael Morikawa <michael.f.morikawa@gmail.com>
2022-10-20 21:04:22 -0700

    Add sql file to create the databases

---
+++ wordle.sql
@@ -0,0 +1,34 @@
+PRAGMA foreign_keys = ON;
+CREATE TABLE IF NOT EXISTS users (
+    userID INT primary key,
+    name VARCHAR,
+    password VARCHAR
+);
+CREATE TABLE IF NOT EXISTS secretWords(word VARCHAR primary key);
+CREATE TABLE IF NOT EXISTS validGuesses(word VARCHAR primary key);
+CREATE TABLE IF NOT EXISTS games (
+    gameID INT primary key,
+    secretWord VARCHAR NOT NULL,
+    userID INT NOT NULL,
+    FOREIGN KEY(userID) references users(userID),
+    FOREIGN KEY(secretWord) references secretWords(word)
+);
+CREATE TABLE IF NOT EXISTS inProgress(
+    gameID INT primary key,
+    guessesMade INT,
+    FOREIGN KEY(gameID) references games(gameID)
+);
+CREATE TABLE IF NOT EXISTS inProgress(
+    gameID INT primary key,
+    guessesMade INT isWinner BOOLEAN,
+    FOREIGN KEY(gameID) references games(gameID)
+);
+CREATE TABLE IF NOT EXISTS guesses(
+    gameID INT primary key,
+    guess VARCHAR,
+    guessNumber SMALLINT,
+    FOREIGN KEY(guess) references validGuesses(word)
+);
+
+
+
