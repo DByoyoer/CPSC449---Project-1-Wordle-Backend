956cdfc90f254f0abd60ab9dbd07a9f0b92e3ae1
Wesley Zoroya <thezoroya@gmail.com>
2022-10-22 15:54:55 -0700

    Uploading broken database version

--- bin/init.sh
+++ bin/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sqlite3 .var/wordle.db < ./share/wordle.sqlite3
+sqlite3 var/wordle.db < ./share/wordle.sql
--- wordle.py
+++ wordle.py
@@ -14,6 +14,15 @@ QuartSchema(app)
 
 app.config.from_file(f"./etc/{__name__}.toml", toml.load)
 
+@dataclasses.dataclass
+class Guess:
+    guess: str
+
+@dataclasses.dataclass
+class User:
+    username: str
+    password: str
+
 async def _get_db():
     db = getattr(g, "_sqlite_db", None)
     if db is None:
@@ -37,17 +46,47 @@ def index():
         """
     )    
 
+@app.route("/login", methods=["GET"])
+async def login():
+    db = await _get_db()
+    await check_user(db, request.authorization)
+    success_response = {"authenticated": True}
+    return success_response, 200
+
+@app.route("/users", methods=["POST"])
+@validate_request(User)
+async def create_user(data):
+    db = await _get_db()
+    user = dataclasses.asdict(data)
+
+    try:
+        await db.execute(
+            """
+                INSERT INTO users(name, password) values (:username, :password)
+            """,
+            user
+        )
+
+    except sqlite3.IntegrityError as e:
+        abort(409, e)
+
+    return {"Message": "User Successfully Created. Please create a game"}, 201
 
 #@app.route("/guess/", methods=["POST"])
-#async def create_guess(guess):
+#@validate_request(Guess)
+#async def create_guess(data):
+    #db = await _get_db()
+    #if validate_input(data, db):
+        
+
     
 
 
-def wordle():
+def wordle(guess, word):
     print("Welcome to Wordle! The goal is to correctly guess a five letter word. You will have 6 attempts. Each correct letter that is in the correct place in the secret word will be marked with green text. Each letter that is present in the secret word but in the incorrect place will be marked with yellow text. Letters not in the secret word will be marked with black text. Good luck!")
+    done = False
     for i in range (0, 6):
         print("Guesses left: ", 6 - i)
-        guess = user_input()
         if guess == word:
             print("You did it!")
             break
@@ -56,7 +95,7 @@ def wordle():
             count = 0
             for char in guess:
                 if char == word[count]:
-                    result = result + "\033[1;32;40m" + char + "\033[0;37;40m"
+                    result = result + char + "\033[0;37;40m"
                 elif char in word:
                     result = result + "\033[1;33;40m" + char + "\033[0;37;40m"
                 else:
@@ -64,11 +103,29 @@ def wordle():
                 count +=1
             print (result)
 
-def user_input():
-    valid = False
-    while not valid:
-        guess = input("Guess the word!  ")
-        if len(guess) != 5:
-            print("Guess must be 5 letters, please try again")
+def validate_input(guess, db):
+    if len(guess) != 5:
+        print("Error: Expected 5 letter word")
+        return False   
+    if not db.fetch_one("SELECT * FROM validGuesses WHERE word=(?)", (guess,)) is None:
+        print("Error: Please enter a valid word")
     return guess
+
+async def check_user(db, auth):
+    if auth is not None:
+        print(auth.type + auth.username + auth.password)
+
+    if auth is not None and auth.type == 'basic':
+        result = await db.fetch_one("SELECT user_id FROM users where username = :username and password =:password",
+                                    values={"username": auth.username, "password": auth.password})
+        print('inside auth')
+        app.logger.debug(type(result))
+        if result:
+            return result.user_id
+        else:
+            print('inside abort')
+            abort(401)
+    else:
+        abort(401)
+
 
