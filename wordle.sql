443e825944d5e5f763de625cf8536f8c116da8e3
Michael Morikawa <michael_f_morikawa@csu.fullerton.edu>
2022-10-21 17:28:34 -0700

    Edit games table to include guessesMade, removed guessesMade from inProgress & finished

--- wordle.sql
+++ wordle.sql
@@ -11,18 +11,18 @@ CREATE TABLE IF NOT EXISTS validGuesses(word VARCHAR primary key);
 CREATE TABLE IF NOT EXISTS games (
     gameID INT primary key,
     secretWord VARCHAR NOT NULL,
+    guessesMade INT DEFAULT 0,
     userID INT NOT NULL,
     FOREIGN KEY(userID) references users(userID),
     FOREIGN KEY(secretWord) references secretWords(word)
 );
 CREATE TABLE IF NOT EXISTS inProgress(
     gameID INT primary key,
-    guessesMade INT,
     FOREIGN KEY(gameID) references games(gameID)
 );
 CREATE TABLE IF NOT EXISTS inProgress(
     gameID INT primary key,
-    guessesMade INT isWinner BOOLEAN,
+    isWinner BOOLEAN,
     FOREIGN KEY(gameID) references games(gameID)
 );
 CREATE TABLE IF NOT EXISTS guesses(
