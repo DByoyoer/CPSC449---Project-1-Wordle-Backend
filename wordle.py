b74d6480259d0eff961a5d5506929c7a29e75a99
Wesley Zoroya <thezoroya@gmail.com>
2022-10-22 18:00:40 -0700

    1st iteration of guess route

--- wordle.py
+++ wordle.py
@@ -22,6 +22,10 @@ class Guess:
 class User:
     username: str
     password: str
+    
+@dataclasses.dataclass
+class Guess:
+    Guess: str
 
 async def _get_db():
     db = getattr(g, "_sqlite_db", None)
@@ -72,56 +76,72 @@ async def create_user(data):
 
     return {"Message": "User Successfully Created. Please create a game"}, 201
 
-#@app.route("/guess/", methods=["POST"])
-#@validate_request(Guess)
-#async def create_guess(data):
-    #db = await _get_db()
-    #if validate_input(data, db):
-        
+@app.route("/games/<int:gameID>", methods=["POST"])
+@validate_request(Guess)
+async def create_guess(data, gameID):
+    db = await _get_db()
+    guess = data
+    try: 
+        await db.execute(
+        """
+            UPDATE games SET guessesMade = guessesMade + 1 WHERE gameID = :gameID", VALUES (:gameID)
+        """,
+            gameID
+        )
+    except sqlite3.IntegrityError as e:
+            abort(409, e)     
 
-    
+    game = dict(await db.fetch_one("SELECT * FROM games WHERE gameID = :gameID", values={"gameID": gameID}))
+    if validate_input(guess, db):
+        try: 
+            await db.execute(
+            """
+                INSERT INTO guesses(guess), values (:guess, :guessNumber)
+            """,
+            guess, game.guessesMade
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409, e)    
+        wordle(guess,game)
 
+def wordle(guess, game):
 
-def wordle(guess, word):
-    print("Welcome to Wordle! The goal is to correctly guess a five letter word. You will have 6 attempts. Each correct letter that is in the correct place in the secret word will be marked with green text. Each letter that is present in the secret word but in the incorrect place will be marked with yellow text. Letters not in the secret word will be marked with black text. Good luck!")
-    done = False
-    for i in range (0, 6):
-        print("Guesses left: ", 6 - i)
-        if guess == word:
-            print("You did it!")
-            break
-        result = ""
-        if not done:
-            count = 0
-            for char in guess:
-                if char == word[count]:
-                    result = result + char + "\033[0;37;40m"
-                elif char in word:
-                    result = result + "\033[1;33;40m" + char + "\033[0;37;40m"
-                else:
-                    result = result + char
-                count +=1
-            print (result)
+    if guess == game.secretWord:
+        #change inprogress to finished table, set is winner to true
+    elif game.guessesMade == 6:
+        #change inprogress to finished table
+    result = []
+    count = 0
+    for char in guess:
+        if char == game.secretWord[count]:
+            result[count] = 2
+        elif char in game.secretWord:
+            result[count] = 1
+        else:
+            result[count] = 0
+        count += 1
+    print (result)
 
-def validate_input(guess, db):
+async def validate_input(guess, db):
     if len(guess) != 5:
         print("Error: Expected 5 letter word")
         return False   
-    if not db.fetch_one("SELECT * FROM validGuesses WHERE word=(?)", (guess,)) is None:
+    if not await db.fetch_one("SELECT * FROM validGuesses WHERE word=(?)", (guess,)) is None:
         print("Error: Please enter a valid word")
-    return guess
+        return False
+    return True
 
 async def check_user(db, auth):
     if auth is not None:
         print(auth.type + auth.username + auth.password)
 
     if auth is not None and auth.type == 'basic':
-        result = await db.fetch_one("SELECT user_id FROM users where username = :username and password =:password",
+        result = await db.fetch_one("SELECT userID FROM users where name = :username and password =:password",
                                     values={"username": auth.username, "password": auth.password})
         print('inside auth')
         app.logger.debug(type(result))
         if result:
-            return result.user_id
+            return result.userID
         else:
             print('inside abort')
             abort(401)
